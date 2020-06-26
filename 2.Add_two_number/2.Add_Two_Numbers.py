4cd6fdd9492ebaf3106a97bb508fbc6bd1ad87a2
Mo <mminouneshan@gmail.com>
2020-06-26 03:53:27 -0500

    second commit

---
+++ 2.Add_two_number/2.Add_Two_Numbers.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:    
+        
+        d1 = l1; i1 = 0; x1 = 0
+        d2 = l2; i2 = 0; x2 = 0
+        x1 += (d1.val)*(10**(i1))
+        x2 += (d2.val)*(10**(i2)) 
+        
+        while True:  
+            if d1.next == None:
+                break
+            i1 +=1
+            d1 = d1.next
+            x1 += (d1.val)*(10**(i1))
+            
+ 
+                
+        while True:
+            if d2.next == None:
+                break
+            i2 +=1
+            d2 = d2.next
+            x2 += (d2.val)*(10**(i2))
+            
+
+                
+        s = x1 + x2     
+        
+        num = list(map(int, str(s)))  
+        num.reverse()
+
+        tail = head = ListNode(num[0])
+        for x in num[1:]:
+            tail.next = ListNode(x) # Create and add another node
+            tail = tail.next 
+
+            
+        return(head)
---
+++ 3.Longest_Substring _Without _Repeating _Characters/3.Longest _Substring_Without_Repeating_Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = []
+        maximum = 0
+        count = 0 
+        for i in range(len(s)):
+            if  s[i] not in l:
+                l.append(s[i])
+                count += 1
+                if count > maximum:
+                    maximum = count
+            else:
+                l = l[l.index(s[i])+1:]
+                l.append(s[i])
+                count = len(l)
+        return maximum
+    
