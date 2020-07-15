4db00878f15e0744e295397e8eea81eed78eb1bc
Mo <mminouneshan@gmail.com>
2020-07-15 10:37:51 -0500

    raneming

---
+++ add_two_number/test_add_two_nums.py
@@ -0,0 +1,20 @@
+from add_two_number import addTwoNumbers
+
+
+class ListNode:
+    def __init__(self, data):
+        "constructor to initiate this object"
+        
+        # store data
+        self.data = data
+        
+        # store reference (next item)
+        self.next = None
+        return
+    
+    def has_value(self, value):
+        "method to compare the value with the node data"
+        if self.data == value:
+            return True
+        else:
+            return False
